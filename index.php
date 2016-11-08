6bfa59c0a97334c0600ae2d6040e0500f4adad84
talibehackeur <hackeurtalibe@gmail.com>
2016-11-08 00:10:02 +0000

    test charge

--- index.php
+++ index.php
@@ -6,55 +6,32 @@
  * Time: 01:57
  */
 require __DIR__ . "/vendor/autoload.php";
+use Stripe\Charge;
 use Stripe\Customer;
 use Stripe\Error;
 use Stripe\Error\Base;
+use Stripe\Error\Card;
 use Stripe\Stripe;
 
 Stripe::setApiKey('sk_test_RjUUjoOIph5e4ddYJUzuV7yB');
 if ($_SERVER['REQUEST_URI'] === '/index.php/charge' or $_SERVER['REQUEST_URI'] === '/charge') {
-    http_response_code(200);
-    echo json_encode(array("lol"=>"polll"));
-}
-
-/*
- *     //Attach a new payment source to the Customer for the currently-logged in user
-    $customer_id = "1234"; // Load the Stripe Customer ID for your logged in user
-
+    $inputJSON = file_get_contents('php://input');
+    $input = json_decode($inputJSON, TRUE); //convert JSON into array
+    $token = $input['token'];
+    $amount = $input['amount'];
+    $currency = $input['currency'];
+    $description = $input['description'];
     try {
-        $customer = \Stripe\Customer::retrieve($customer_id);
-        $customer->sources->create(array("source" => $_POST["source"]));
-        http_response_code(200);
-    } catch (Base $e) {
-        var_dump($e->getMessage());
-        http_response_code(402);
-    }
-} elseif ($_SERVER['REQUEST_URI'] === '/index.php/customer/default_source') {
-    //This is called when the user changes their selected payment method in our UI. For this
-    $customer_id = "13323123";
-
-    try {
-        $customer = \Stripe\Customer::retrieve($customer_id);
-        $customer->default_source = $_GET["default_source"];
-        $customer->save();
-        http_response_code(200);
-    } catch (Base $e) {
-        var_dump($e->getMessage());
-        http_response_code(402);
-    }
-
-} elseif ($_SERVER['REQUEST_URI'] === '/index.php/customer') {
-    //Retrieve the Customer object for the currently logged-in use
-    try {
-        $customer_id = Customer::create(array(
-            "description" => "Customer for avery.williams@example.com",
+        $charge = Charge::create(array(
+            "amount" => (int)$amount * 100,
+            "currency" => $currency,
+            "source" => $token,
+            "description" => $description
         ));
-        $customer = Customer::retrieve($customer_id->id);
-        header('Content-Type: application/json');
-        echo $customer->jsonSerialize();
-    } catch (Base $e) {
-        var_dump($e->getMessage());
-        http_response_code(402);
+        http_response_code(200);
+    } catch (Card $e) {
+        http_response_code(400);
+        return -1;
     }
 }
- */
+
